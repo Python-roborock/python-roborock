93c7be1bafd81f2db0ae2f1b89c5c2cca4effdcf
Luke <conway220@gmail.com>
2025-08-10 21:18:29 -0400

    chore: comment

--- roborock/clean_modes.py
+++ roborock/clean_modes.py
@@ -62,17 +62,22 @@ class WaterModes(RoborockModeEnum):
 
 
 def get_clean_modes(features: DeviceFeatures) -> list[CleanModes]:
+    """Get the valid clean modes for the device - also known as 'fan power' or 'suction mode'"""
     modes = [CleanModes.QUIET, CleanModes.BALANCED, CleanModes.TURBO, CleanModes.MAX]
     if features.is_max_plus_mode_supported or features.is_none_pure_clean_mop_with_max_plus:
+        # If the vacuum has max plus mode supported
         modes.append(CleanModes.MAX_PLUS)
     if features.is_pure_clean_mop_supported:
+        # If the vacuum is capable of 'pure mop clean' aka no vacuum
         modes.append(CleanModes.OFF)
     else:
+        # If not, we can add gentle
         modes.append(CleanModes.GENTLE)
     return modes
 
 
 def get_clean_routes(features: DeviceFeatures, region: str) -> list[CleanRoutes]:
+    """The routes that the vacuum will take while mopping"""
     if features.is_none_pure_clean_mop_with_max_plus:
         return [CleanRoutes.FAST, CleanRoutes.STANDARD]
     supported = [CleanRoutes.STANDARD, CleanRoutes.DEEP]
@@ -82,6 +87,7 @@ def get_clean_routes(features: DeviceFeatures, region: str) -> list[CleanRoutes]
             and features.is_clean_route_deep_slow_plus_supported
             and region == "CN"
         ):
+            # for some reason there is a china specific deep plus mode
             supported.append(CleanRoutes.DEEP_PLUS_CN)
         else:
             supported.append(CleanRoutes.DEEP_PLUS)
@@ -92,12 +98,15 @@ def get_clean_routes(features: DeviceFeatures, region: str) -> list[CleanRoutes]
 
 
 def get_water_modes(features: DeviceFeatures) -> list[WaterModes]:
+    """Get the valid water modes for the device - also known as 'water flow' or 'water level'"""
     supported_modes = [WaterModes.OFF]
     if features.is_mop_shake_module_supported:
+        # For mops that have the vibrating mop pad, they do mild standard intense
         supported_modes.extend([WaterModes.MILD, WaterModes.STANDARD, WaterModes.INTENSE])
     else:
         supported_modes.extend([WaterModes.LOW, WaterModes.MEDIUM, WaterModes.HIGH])
     if features.is_custom_water_box_distance_supported:
+        # This is for devices that allow you to set a custom water flow from 0-100
         supported_modes.append(WaterModes.CUSTOM)
     if features.is_mop_shake_module_supported and features.is_mop_shake_water_max_supported:
         supported_modes.append(WaterModes.EXTREME)
