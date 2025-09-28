2e7c2bd3dbddc006be4ba0dc8182eb428ad1277a
Allen Porter <allen.porter@gmail.com>
2025-09-27 18:44:59 -0700

    feat: add pdoc for leveraging python docstrings for documentation

--- roborock/__init__.py
+++ roborock/__init__.py
@@ -1,7 +1,26 @@
-"""Roborock API."""
+"""Roborock API.
+
+.. include:: ../README.md
+"""
 
 from roborock.b01_containers import *
 from roborock.code_mappings import *
 from roborock.containers import *
 from roborock.exceptions import *
 from roborock.roborock_typing import *
+
+__all__ = [
+    "web_api",
+    "version_1_apis",
+    "version_a01_apis",
+    "containers",
+    "b01_containers",
+    "const",
+    "cloud_api",
+    "clean_modes",
+    "code_mappings",
+    "roborock_typing",
+    "exceptions",
+
+    # We'll add new APIs here in the future e.g. devices/
+]
