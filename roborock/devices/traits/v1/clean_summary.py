f362064a86387b40672c46f536fc2cd708e1262f
Allen Porter <allen.porter@gmail.com>
2025-11-10 19:30:26 -0800

    chore: Revert unnecessary change to Clean Summary Trait

--- roborock/devices/traits/v1/clean_summary.py
+++ roborock/devices/traits/v1/clean_summary.py
@@ -60,16 +60,18 @@ class CleanSummaryTrait(CleanSummaryWithDetail, common.V1TraitMixin):
             if isinstance(response[-1], dict):
                 records = [CleanRecord.from_dict(rec) for rec in response]
                 final_record = records[-1]
-                # Aggregate basic stats from prior partial records (exclude last which is full)
-                final_record.begin = records[0].begin
-                final_record.start_type = records[0].start_type
-                for rec in records[0:-1]:
-                    final_record.duration = (final_record.duration or 0) + (rec.duration or 0)
-                    final_record.area = (final_record.area or 0) + (rec.area or 0)
-                    final_record.avoid_count = (final_record.avoid_count or 0) + (rec.avoid_count or 0)
-                    final_record.wash_count = (final_record.wash_count or 0) + (rec.wash_count or 0)
-                return final_record
-            # There are still a few unknown variables in this.
+                try:
+                    # This code is semi-presumptuous - so it is put in a try finally to be safe.
+                    final_record.begin = records[0].begin
+                    final_record.start_type = records[0].start_type
+                    for rec in records[0:-1]:
+                        final_record.duration = (final_record.duration or 0) + (rec.duration or 0)
+                        final_record.area = (final_record.area or 0) + (rec.area or 0)
+                        final_record.avoid_count = (final_record.avoid_count or 0) + (rec.avoid_count or 0)
+                        final_record.wash_count = (final_record.wash_count or 0) + (rec.wash_count or 0)
+                except Exception:
+                    # Return final record when an exception occurred
+                    return final_record            # There are still a few unknown variables in this.
             begin, end, duration, area = unpack_list(response, 4)
             return CleanRecord(begin=begin, end=end, duration=duration, area=area)
         raise ValueError(f"Unexpected clean record format: {response!r}")
