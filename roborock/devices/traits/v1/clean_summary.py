b07b2a3ffc307715d5e7f59a8632b514ead8751e
Allen Porter <allen.porter@gmail.com>
2025-11-10 19:17:33 -0800

    fix: Update trait `refresh` method to return `None`

--- roborock/devices/traits/v1/clean_summary.py
+++ roborock/devices/traits/v1/clean_summary.py
@@ -14,7 +14,7 @@ class CleanSummaryTrait(CleanSummaryWithDetail, common.V1TraitMixin):
 
     command = RoborockCommand.GET_CLEAN_SUMMARY
 
-    async def refresh(self) -> Self:
+    async def refresh(self) -> None:
         """Refresh the clean summary data and last clean record.
 
         Assumes that the clean summary has already been fetched.
@@ -23,10 +23,9 @@ class CleanSummaryTrait(CleanSummaryWithDetail, common.V1TraitMixin):
         if not self.records:
             _LOGGER.debug("No clean records available in clean summary.")
             self.last_clean_record = None
-            return self
+            return
         last_record_id = self.records[-1]
         self.last_clean_record = await self.get_clean_record(last_record_id)
-        return self
 
     @classmethod
     def _parse_type_response(cls, response: common.V1ResponseData) -> Self:
@@ -61,23 +60,15 @@ class CleanSummaryTrait(CleanSummaryWithDetail, common.V1TraitMixin):
             if isinstance(response[-1], dict):
                 records = [CleanRecord.from_dict(rec) for rec in response]
                 final_record = records[-1]
-                try:
-                    # This code is semi-presumptuous - so it is put in a try finally to be safe.
-                    final_record.begin = records[0].begin
-                    final_record.begin_datetime = records[0].begin_datetime
-                    final_record.start_type = records[0].start_type
-                    for rec in records[0:-1]:
-                        final_record.duration = (final_record.duration or 0) + (rec.duration or 0)
-                        final_record.area = (final_record.area or 0) + (rec.area or 0)
-                        final_record.avoid_count = (final_record.avoid_count or 0) + (rec.avoid_count or 0)
-                        final_record.wash_count = (final_record.wash_count or 0) + (rec.wash_count or 0)
-                        final_record.square_meter_area = (final_record.square_meter_area or 0) + (
-                            rec.square_meter_area or 0
-                        )
-                    return final_record
-                except Exception:
-                    # Return final record when an exception occurred
-                    return final_record
+                # Aggregate basic stats from prior partial records (exclude last which is full)
+                final_record.begin = records[0].begin
+                final_record.start_type = records[0].start_type
+                for rec in records[0:-1]:
+                    final_record.duration = (final_record.duration or 0) + (rec.duration or 0)
+                    final_record.area = (final_record.area or 0) + (rec.area or 0)
+                    final_record.avoid_count = (final_record.avoid_count or 0) + (rec.avoid_count or 0)
+                    final_record.wash_count = (final_record.wash_count or 0) + (rec.wash_count or 0)
+                return final_record
             # There are still a few unknown variables in this.
             begin, end, duration, area = unpack_list(response, 4)
             return CleanRecord(begin=begin, end=end, duration=duration, area=area)
