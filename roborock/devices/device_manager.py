c171ff86a4271bcb95d0ea50543ed8033e4284c1
Luke <conway220@gmail.com>
2025-12-05 21:17:48 -0500

    fix: use strip not split

--- roborock/devices/device_manager.py
+++ roborock/devices/device_manager.py
@@ -197,17 +197,15 @@ async def create_device_manager(
                 trait = a01.create(product, channel)
             case DeviceVersion.B01:
                 channel = create_mqtt_channel(user_data, mqtt_params, mqtt_session, device)
-                if "ss" in product.model.strip(".")[-1]:
+                if "ss" in product.model.split(".")[-1]:
                     raise NotImplementedError(
                         f"Device {device.name} has unsupported version B01_{product.model.strip('.')[-1]}"
                     )
-                elif "sc" in product.model.strip(".")[-1]:
+                elif "sc" in product.model.split(".")[-1]:
                     # Q7 devices start with 'sc' in their model naming.
                     trait = b01.q7.create(channel)
                 else:
-                    raise NotImplementedError(
-                        f"Device {device.name} has unsupported B01 model: {product.model}"
-                    )
+                    raise NotImplementedError(f"Device {device.name} has unsupported B01 model: {product.model}")
             case _:
                 raise NotImplementedError(f"Device {device.name} has unsupported version {device.pv}")
         return RoborockDevice(device, product, channel, trait)
