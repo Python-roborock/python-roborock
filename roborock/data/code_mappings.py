7024dba00ef8386b6b4a7da442694e6baecc3fdf
Luke <conway220@gmail.com>
2025-12-16 16:50:44 -0500

    fix: use value instead of name to get lower cased

--- roborock/data/code_mappings.py
+++ roborock/data/code_mappings.py
@@ -73,7 +73,7 @@ class RoborockModeEnum(StrEnum):
     @classmethod
     def keys(cls) -> list[str]:
         """Returns a list of all member names."""
-        return [member.name for member in cls]
+        return [member.value for member in cls]
 
 
 ProductInfo = namedtuple("ProductInfo", ["nickname", "short_models"])
