532f108667db3e43867da8c0ab06a6490175f4cc
Allen Porter <allen.porter@gmail.com>
2026-03-08 19:50:52 -0700

    Fix: Make `current_rooms` property return `None` if `current_map_data` is `None`.

--- roborock/devices/traits/v1/home.py
+++ roborock/devices/traits/v1/home.py
@@ -222,8 +222,10 @@ class HomeTrait(RoborockBase, common.V1TraitMixin):
         return self._home_map_info.get(current_map_flag)
 
     @property
-    def current_rooms(self) -> list[NamedRoomMapping]:
+    def current_rooms(self) -> list[NamedRoomMapping] | None:
         """Returns the room names for the current map."""
+        if self.current_map_data is None:
+            return None
         return self.current_map_data.rooms
 
     @property
