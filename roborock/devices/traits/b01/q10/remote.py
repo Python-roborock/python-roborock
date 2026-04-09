dc86444d4922b9034ab7d79626a7aeea29499d4a
Kraina68512 <mardahu.illyahu1@gmail.com>
2026-04-09 18:53:29 +0300

    Deleted command Ids from remote.py Added commands enum to b01_q10_code_mappings.py Created pytest file

--- roborock/devices/traits/b01/q10/remote.py
+++ roborock/devices/traits/b01/q10/remote.py
@@ -1,58 +1,44 @@
 """Traits for Q10 B01 devices."""
 
 from roborock.data.b01_q10.b01_q10_code_mappings import (
-    B01_Q10_DP
+    B01_Q10_DP,
+    RemoteCommand,
 )
 from typing import Any
 
 from .command import CommandTrait
 
-REMOTE_COMMANDS = {
-    "forward": 0,
-    "left": 2,
-    "right": 3,
-    "stop": 4,
-    "exit": 5,
-}
 
 class RemoteTrait:
     """Trait for sending vacuum commands.
 
-    This is a wrapper around the CommandTrait for sending vacuum related
+    This is a wrapper around the CommandTrait for sending remote related
     commands to Q10 devices.
     """
 
     def __init__(self, command: CommandTrait) -> None:
-        """Initialize the VacuumTrait."""
+        """Initialize the RemoteTrait."""
         self._command = command
 
-    async def send_common_dp(self, command: B01_Q10_DP, value: Any) -> None:
-        await self._command.send(B01_Q10_DP.COMMON, params={command.code: value})
-
-    async def send_remote(self, action: str) -> None:
-        await self.send_common_dp(B01_Q10_DP.REMOTE, REMOTE_COMMANDS[action])
+    async def _send_remote(self, action: RemoteCommand) -> None:
+        await self._command.send(B01_Q10_DP.COMMON, params={B01_Q10_DP.REMOTE: action.value})
 
     async def forward(self) -> None:
         """Move forward."""
-        
-        await self.send_remote("forward")
+        await self._send_remote(RemoteCommand.FORWARD)
 
     async def left(self) -> None:
         """Turn left."""
-        
-        await self.send_remote("left")
+        await self._send_remote(RemoteCommand.LEFT)
 
     async def right(self) -> None:
         """Turn right."""
-        
-        await self.send_remote("right")
+        await self._send_remote(RemoteCommand.RIGHT)
 
     async def stop(self) -> None:
         """Stop last moving command or start remote control."""
-        
-        await self.send_remote("stop")
+        await self._send_remote(RemoteCommand.STOP)
     
     async def exit(self) -> None:
         """Exit remote control."""
-        
-        await self.send_remote("exit")
+        await self._send_remote(RemoteCommand.EXIT)
